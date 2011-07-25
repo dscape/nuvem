354a872656c6828b974c9e91cad5c9b9b8d34eb8
Nuno Job <nunojobpinto@gmail.com>
2011-07-24 21:35:50 -0700

    some style changes

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-module.exports = require('./lib/nuvem/');
+module.exports = require('./lib/nuvem');
--- sample/simple.js
+++ sample/simple.js
@@ -1,5 +1,5 @@
 require.paths.unshift('./lib');
 
 var nuvem = require('nuvem');
-var db    = nuvem('http://user:pass@localhost:123/bla');
-db.configure('http://user:pass@localhost:123/bla');
+var db    = nuvem('http://user:pass@localhost:123');
+db.configure('http://user:pass@localhost:123');
