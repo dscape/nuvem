2a9c96032a9347c38ae5eb47cc2523e9309e8857
Nuno Job <nunojobpinto@gmail.com>
2011-07-25 00:08:50 -0700

    finished the 4 verbs. yay

--- sample/simple.js
+++ sample/simple.js
@@ -3,19 +3,21 @@ require.paths.unshift('./lib');
 var nuvem = require('nuvem');
 var db    = nuvem('http://user:pass@localhost:123');
 db.configure('http://admin:admin@localhost:4380');
-db.document.get("a", function (err, document ) {
+db.json.get("a", function (err, document ) {
   console.log(err, document);
 });
-db.document.insert("a", "",
-  function (err) {
-    console.log(err);
+db.json.insert("a", "", function (err) {
+  if(err) console.log(err);
 });
-db.document.insert("a", {"some": "json"},
-  function (err) {
-    console.log(err);
+db.json.insert("a", {"some": "trash"}, function (err) {
+  if(err) console.log(err);
 });
-db.document.insert("a", {some: "json"},
-  function (err) {
-    console.log(err);
+db.json.insert("a", {some: "bar"}, function (err) {
+  if(err) console.log(err);
 });
-try { JSON.parse("%$") } catch(e) { console.log(e) }
+db.json.delete("a", function (err) {
+  if(err) console.log(err);
+});
+db.json.update("a", {blerh: "foo"}, function (err) {
+  if(err) console.log(err);
+});
