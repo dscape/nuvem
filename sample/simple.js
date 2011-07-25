019cf923c1b8cd3670b5c58737defa9cc928df14
Nuno Job <nunojobpinto@gmail.com>
2011-07-24 23:33:48 -0700

    added store and get json

--- sample/simple.js
+++ sample/simple.js
@@ -2,4 +2,20 @@ require.paths.unshift('./lib');
 
 var nuvem = require('nuvem');
 var db    = nuvem('http://user:pass@localhost:123');
-db.configure('http://user:pass@localhost:123');
+db.configure('http://admin:admin@localhost:4380');
+db.document.get("a", function (err, document ) {
+  console.log(err, document);
+});
+db.document.insert("a", "",
+  function (err) {
+    console.log(err);
+});
+db.document.insert("a", {"some": "json"},
+  function (err) {
+    console.log(err);
+});
+db.document.insert("a", {some: "json"},
+  function (err) {
+    console.log(err);
+});
+try { JSON.parse("%$") } catch(e) { console.log(e) }
