3564b0371c7782567c5b5b9347fe44e3c24ecad4
Nuno Job <nunojobpinto@gmail.com>
2011-08-08 18:16:58 +0100

    bump to version 0.0.4

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports = exports = require('./lib/nuvem');
-exports.version = JSON.parse(require('fs').readFileSync("package.json"))
+exports.version = JSON.parse(require('fs').readFileSync(__dirname + "/package.json"))
   .version;
 exports.path     = __dirname;
