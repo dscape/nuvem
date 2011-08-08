dfd9b49baf4da34c1b11c3d4abfcee0f2c80911a
Nuno Job <nunojobpinto@gmail.com>
2011-08-08 18:15:13 +0100

    bump version

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 module.exports = exports = require('./lib/nuvem');
-exports.version = "0.0.3";
+exports.version = JSON.parse(require('fs').readFileSync("package.json"))
+  .version;
 exports.path     = __dirname;
