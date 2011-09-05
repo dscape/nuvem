765a7256ca8c68d5dcaf35835ac8da9d05165810
Nuno Job <nunojobpinto@gmail.com>
2011-09-05 16:22:35 +0100

    added support for directory and collection queries

--- test/json/find-qs.js
+++ test/json/find-qs.js
@@ -6,28 +6,66 @@ var ensure = require('ensure')
   , db     = nuvem(cfg)
   , tests = exports;
 
+function setup(salt,collections) {
+  var params = {};
+  salt = salt || "";
+  if (collections) { params.collection = collections; }
+  return [ 
+    function(callback) { db.json.insert("/foo/bar/foobar" + salt,
+      {"foo": "fox in the snow"}, params, callback); }
+  , function(callback) { db.json.insert("/foo/bar/barfoo" + salt,
+      {"bar": "where do you go"}, params, callback); }
+  , function(callback) { db.json.insert("/foo/another" + salt,
+      {"foo": "to find something you could eat fox"}, params, callback); }
+  ];
+}
+
+function teardown(salt) {
+  return [
+    function(callback) { db.json.destroy("/foo/bar/foobar" + salt, callback); }
+  , function(callback) { db.json.destroy("/foo/bar/barfoo" + salt, callback); }
+  , function(callback) { db.json.destroy("/foo/another" + salt, callback); }
+  ];
+}
+
 tests.snow = function (cb) {
-  var setup = 
-    [ function(callback) { db.json.insert("foobar",  {"foo": "fox in the snow"}, callback); }
-    , function(callback) { db.json.insert("barfoo",  {"bar": "where do you go"}, callback); }
-    , function(callback) { db.json.insert("another", {"foo": "to find something you could eat"}, callback); }
-    ];
-  async.parallel(setup, function(e){
+  async.parallel(setup('_snow'), function(e) {
     if(e) { throw e; }
     db.json.first('snow', cb);
   });
 };
 
 tests.snow_ok = function(e,b,h) {
-  var teardown = 
-    [ function(callback) { db.json.destroy("foobar", callback); }
-    , function(callback) { db.json.destroy("barfoo", callback); }
-    , function(callback) { db.json.destroy("another", callback); }
-    ];
   if(e) { throw e; }
   assert.equal(h["status-code"],200);
-  assert.equal(b.uri, "/foobar");
-  async.parallel(teardown);
+  assert.ok(b.uri.indexOf("/foo/bar/foobar") !== -1);
+  async.parallel(teardown('_snow'));
+};
+
+tests.collections = function (cb) {
+  async.parallel(setup('_collections', ['dog','red']), function(e){
+    if(e) { throw e; }
+    db.json.find('snow', { include: 'all' }, cb);
+  });
+};
+
+tests.collections_ok = function (e,b,h) {
+  assert.isNull(e);
+  assert.ok(b.collections.indexOf('red')!==-1);
+  async.parallel(teardown('_collections'));
+};
+
+tests.directories = function (cb) {
+  async.parallel(setup('_directories'), function(e){
+    if(e) { throw e; }
+    db.json.find('fox', { inDirectory: '/foo' }, cb);
+  });
+};
+
+tests.directories_ok = function (e,b,h) {
+  assert.isNull(e);
+  assert.ok(b.uri.indexOf("another") !== -1);
+  async.parallel(teardown('_directories'));
 };
 
 ensure(__filename, tests, module);
